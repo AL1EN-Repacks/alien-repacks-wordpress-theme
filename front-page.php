fb57e781e3f564633c3acbc560a42b7580674041
AL1EN <107768574+AL1EN-Repacks@users.noreply.github.com>
2022-06-19 02:37:51 +0600

    Add files via upload

---
+++ front-page.php
@@ -0,0 +1,40 @@
+<!Doctype html>
+<html>
+<!––
+AL1EN Repacks
+Version : 0.1
+Author : AL1EN
+Github : https://github.com/AL1EN-Repacks/alien-repacks.site
+––>
+
+<head>
+<link rel="stylesheet" href="css/style.css">
+
+<title> AL1EN Repacks </title>
+
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</head>
+
+<body>
+
+	<div class="sidebar">
+		<header>AL1EN Repacks</header>
+
+		<ul>
+			<li><a href="#"><i class="fas fa-qrcode"></i>Lattest</li>
+			<li><a href="#"><i class="fas fa-stream"></i>A-Z List</li>
+			<li><a href="#"><i class="fas fa-link"></i>Forum</li>
+			<li><a href="#"><i class="fas fa-envelope"></i>Theme</li>
+		</ul>
+	</div>
+
+<div>
+	<h1>Title</h1>
+	<p>This is a template</p>
+</div>
+
+
+</body>
+
+
+</html>
